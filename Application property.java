d24a33f2de9f2e204c55b11754c7b610528999fe
mohinikapse <mohinikapse02@gmail.com>
2025-08-20 09:17:51 +0530

    Create Application property.java

---
+++ Application property.java
@@ -0,0 +1,6 @@
+spring.datasource.url=jdbc:postgresql:
+spring.datasource.username=postgres
+spring.datasource.password=YOUR_PASSWORD
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+server.port=8080
